ddab91bfbba3fc714004846dddb0bd2df8714325
Christopher Black <Huckpilot@Christophers-MacBook-Pro.local>
2016-05-19 10:02:19 -0700

    Moved things around.

---
+++ eyes.js
@@ -0,0 +1,56 @@
+var DrawEye = function(eyecontainer, pupil, eyeposx, eyeposy, eyer){
+    $("#monstereyes").append("<div id='" + eyecontainer + "'><div id='" + pupil + "'></div></div>");
+
+    eyecontainer = "#" + eyecontainer;
+    pupil = "#" + pupil;
+
+    $(eyecontainer).css({left:eyeposx, top:eyeposy});
+    $(pupil).css({width:eyer*0.4,height:eyer*0.4});
+    $(eyecontainer).css({width:eyer,height:eyer});
+    $(pupil).css({position: 'relative', background: '#000000', 'border-radius':'50%'});
+    $(eyecontainer).css({position:'absolute', background:'#FFFFFF', overflow:'hidden', 'border-radius': '50%'});
+
+    // Initialise core variables
+    var r = $(pupil).width()/2;
+    var center = {
+        x: $(eyecontainer).width()/2 - r, 
+        y: $(eyecontainer).height()/2 - r
+    };
+    var distanceThreshold = $(eyecontainer).width()/2 - r;
+    var mouseX = center.x, mouseY = center.y;
+
+    // Listen for mouse movement
+    $(document).mousemove(function(e){ 
+    var d = {
+          x: e.clientX - r - eyeposx - center.x,
+          y: e.clientY - r - eyeposy - center.y
+    };
+    // console.log(d);
+    var distance = Math.sqrt(d.x*d.x + d.y*d.y);
+    if (distance < distanceThreshold) {
+          mouseX = e.pageX - eyeposx - r;
+          mouseY = e.pageY - eyeposy - r;
+    } else {
+          mouseX = d.x / distance * distanceThreshold + center.x;
+          mouseY = d.y / distance * distanceThreshold + center.y;
+    }
+    // console.log(mouseX, e.pageX);
+    });
+
+    // Update pupil location
+    pupil = $(pupil);
+    var xp = center.x, yp = center.y;
+    var loop = setInterval(function(){
+        // change 1 to alter damping/momentum - higher is slower
+        xp += (mouseX - xp) / 1;
+        yp += (mouseY - yp) / 1;
+        pupil.css({
+            left: xp,
+            top:yp
+        });
+    }, 1);
+};
+
+// console.log($(window).width() / 2);
+var eye = new DrawEye("eye1", "pupil1", 2, 17, 13 );
+var eye = new DrawEye("eye2", "pupil2", 25, 17, 13);
